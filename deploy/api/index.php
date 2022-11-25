f1d7af49ef58b56ef089b32765acad3c2ce893e8
Florian <fmetz@capcod.eu>
2022-11-25 18:24:28 +0100

    lzekfkebverv

--- deploy/api/index.php
+++ deploy/api/index.php
@@ -63,7 +63,7 @@ $app->get('/api/hello/{name}', function (Request $request, Response $response, $
     return $response;
 });
 
-$app->get('/api/login', function (Request $request, Response $response, $args) {
+$app->post('/api/login', function (Request $request, Response $response, $args) {
 
     $inputJSON = file_get_contents('php://input');
     $body = json_decode( $inputJSON, TRUE ); //convert JSON into array 
